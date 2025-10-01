58d3a93b47d0df522f4c91067ebc85414a17af4a
AkbariAidul <akbariaidul@gmail.com>
2025-10-01 11:39:28 +0800

    Coding CRUD data category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -17,6 +18,12 @@ class Category extends Model
         'slug',
         'icon',
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function news(): HasMany
     {
